8ca2de8b410bd42ece44f81ceed8c94cb9f2afd2
JoniMonetti1 <jonathanmonetti6@gmail.com>
2024-03-14 17:18:28 -0300

    Se avanzo con el proyecto, falta agregar los metodos CRUD a la tabla c

---
+++ src/main/java/com/mycompany/tablas/Conexion.java
@@ -0,0 +1,22 @@
+package com.mycompany.tablas;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private String user = "root";
+    private String base = "tienda";
+    private String password = "";
+    private String url = "jdbc:mysql://localhost:3306/" + base;
+    private Connection con = null;
+    
+    public Connection getConexion() {
+        try {
+            con = DriverManager.getConnection(this.url, this.user, this.password);
+        } catch (SQLException e){
+            System.err.println(e.toString());
+        }
+        return con;
+    }
+}
